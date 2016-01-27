fe2fc1533e17cef82b5f04553bed3f360bf7c152
Grayson Koonce <grayson@uber.com>
2016-01-26 19:20:05 -0800

    tag var

--- scripts/publish-to-docker-registry.sh
+++ scripts/publish-to-docker-registry.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo $PATH
-echo $TRAVIS_BRANCH
-echo goodbye
+echo "$TRAVIS_BRANCH"
+echo "$TRAVIS_BUILD_DIR"
+
+TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH"; fi)
+
+echo "tag is $TAG"
+
+# docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
