8ceaeef4533897f219d6cf770cc5b08a0b9e053d
Grayson Koonce <grayson@uber.com>
2016-01-26 18:32:42 -0800

    output environment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # xlang
+
+[![Build Status](https://travis-ci.org/yarpc/xlang.svg?branch=master)](https://travis-ci.org/yarpc/xlang)
--- main.go
+++ main.go
@@ -1,17 +1,31 @@
+// [Environment variables](http://en.wikipedia.org/wiki/Environment_variable)
+// are a universal mechanism for [conveying configuration
+// information to Unix programs](http://www.12factor.net/config).
+// Let's look at how to set, get, and list environment variables.
+
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"runtime"
-)
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
-
-}
+import "os"
+import "strings"
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+
+	// To set a key/value pair, use `os.Setenv`. To get a
+	// value for a key, use `os.Getenv`. This will return
+	// an empty string if the key isn't present in the
+	// environment.
+	os.Setenv("FOO", "1")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
+
+	// Use `os.Environ` to list all key/value pairs in the
+	// environment. This returns a slice of strings in the
+	// form `KEY=value`. You can `strings.Split` them to
+	// get the key and value. Here we print all the keys.
+	fmt.Println()
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0])
+	}
 }
