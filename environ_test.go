a7e46a2602fcc8fa82558d2f512d424304158520
Grayson Koonce <grayson.koonce@gmail.com>
2016-02-02 11:53:37 -0800

    Update environ_test.go
    spacing

--- environ_test.go
+++ environ_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestReadMatrixFromEnviron(t *testing.T) {
-
 	os.Setenv("XLANG_CLIENTS", "yarpc-go,yarpc-node,yarpc-browser")
 	os.Setenv("XLANG_DIMENSION_SERVER", "yarpc-go,yarpc-node")
 	os.Setenv("XLANG_DIMENSION_TRANSPORT", "http,tchannel")
@@ -16,7 +15,6 @@ func TestReadMatrixFromEnviron(t *testing.T) {
 	matrix := ReadMatrixFromEnviron()
 
 	assert.Equal(t, matrix.Clients, []string{"yarpc-go", "yarpc-node", "yarpc-browser"})
-
 	assert.Equal(t, matrix.Dimensions, []Dimension{
 		{Name: "server", Values: []string{"yarpc-go", "yarpc-node"}},
 		{Name: "transport", Values: []string{"http", "tchannel"}},
