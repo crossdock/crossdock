72f74a6aaf34e397dae470909d78a20a3d567bc0
Grayson Koonce <grayson@uber.com>
2016-02-04 11:37:23 -0800

    output formatting

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
+	fmt.Printf("\nCrossdock starting...\n\n")
 	matrix := ReadMatrixFromEnviron()
 
 	fmt.Printf("Waiting on CROSSDOCK_CLIENTS=%v\n\n", matrix.Clients)
 	Wait(matrix.Clients, time.Duration(30)*time.Second)
 
-	fmt.Println("Begining matrix of tests")
+	fmt.Printf("\nExecuting Matrix...\n\n")
 	results := Execute(matrix)
 
 	Output(results)
